973b760702287470bd072e2465cbef00d394b3ea
samcabezas98 <samcabezas98@gmail.com>
2019-11-07 18:36:48 -0500

    Update R00_IDS03_J.java
    Rule 00. Input Validation and Data Sanitization (IDS)

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -8,11 +8,11 @@ if(loginSuccessful) {
 
 // Correct the code as shown in theCompliant Solution below:
 if(loginSuccessful) {
-	logger.severe(”User login succeeded	for: ” + sanitizeUser (username ));
+	logger.severe(”User login succeeded for: ” + sanitizeUser (username ));
 } else {
   logger.severe(”User login failed for: ” + sanitizeUser (username ));
 }
 public	String	sanitizeUser ( String username) {
 	return Pattern . matches (”[A−Za−z0−9   ]+”, username ))
-		? username	: ”unauthorized	user ”;
+		? username: ”unauthorized	user ”;
 }

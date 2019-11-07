d21b741fd14b3da707bb6abbdf5a941c25ff8faa
samcabezas98 <samcabezas98@gmail.com>
2019-11-07 18:44:03 -0500

    Add files via upload
    2. Rule 02. Expressions (EXP)

---
+++ R02_XP00_J.java
@@ -0,0 +1,19 @@
+// Rule 02. Expressions (EXP)
+// XP00-J. Do not ignore values returned by methods Given the non-compliant code below:
+public	void	deleteFile (){
+
+	File someFile = new File(”someFileName.txt ”);
+    // Do something with someFile
+    someFile . delete ();
+}
+
+// Correct the code as shown in the Compliant Solution below:
+void deleteFile (){
+
+	File	someFile = new File (”someFileName . txt ”);
+    // Do something with someFile
+    if(!someFile.delete ()) {
+	// Handle failure to delete the	file
+    }
+    
+}
